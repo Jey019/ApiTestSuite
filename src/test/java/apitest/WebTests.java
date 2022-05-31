a66f7a4ab3a1a8f7cbc5cb11d41113611744d42a
Jey <jey.venkataramanan@accesshq.com>
2022-05-31 11:26:20 +1000

    Created Modern form tests which is submitted.

--- src/test/java/apitest/WebTests.java
+++ src/test/java/apitest/WebTests.java
@@ -2,9 +2,14 @@ package apitest;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class WebTests {
 
@@ -23,12 +28,41 @@ public class WebTests {
     }
 
     @Test
-    public void PlanetTest()
+    public void FormsTests()
     {
-        WebElement planets = driver.findElement(By.cssSelector("[aria-label='planets']"));
-        planets.click();
-        Assertions.assertEquals("https://d18u5zoaatmpxx.cloudfront.net/#/planets", driver.getCurrentUrl());
+        WebElement forms = driver.findElement(By.cssSelector("[aria-label='forms']"));
+        forms.click();
+        Assertions.assertEquals("https://d18u5zoaatmpxx.cloudfront.net/#/forms", driver.getCurrentUrl());
+        WebElement id = driver.findElement(By.id("name"));
+        id.sendKeys("Jay");
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("jey.venkataramanan@accesshq.com");
+        WebElement state = driver.findElement(By.id("state"));
+        state.sendKeys("NSW");
+        WebElement agree = driver.findElement(By.className("v-input--selection-controls__ripple"));
+        agree.click();
+        WebElement btnfound = null;
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+        for (WebElement btn : buttons)
+        {
+            if (btn.getText().equalsIgnoreCase("submit"))
+            {
+                btnfound = btn;
+                break;
+            }
+
+
+        }
+        if (btnfound == null) { throw (new NotFoundException()); }
+        btnfound.click();
+
+        WebElement popup = driver.findElement(By.className("popup-message"));
+        new WebDriverWait(driver, 10).until(d -> popup.isDisplayed());
+        //WebElement popmessage = driver.findElement(By.cssSelector(".snackbar"));
+        Assertions.assertEquals("Thanks for your feedback Jay", popup.getText());
+
     }
+
 
     @AfterAll
     public static void Quit(){
